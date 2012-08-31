c00e0ea1d23d4244f3ed74d603831b6591ee1048
blindwonder <blindwonder@gmail.com>
2012-08-31 11:43:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opensourcebjj::Application.config.secret_token = 'dbd43e95b05f1603c650fd764985f794f82af64c8f66b0b78ca6a3a60c0d2fd9328d8d4f3efc3cfa29a594ce942549b7a859a83482c48710df47acb85d217c56'
